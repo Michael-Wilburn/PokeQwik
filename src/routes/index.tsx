fa9a3972d7492cee140a650a71f1ebfe599d59d8
Michael Wilburn <michael-mw@hotmail.com>
2023-08-06 21:36:16 -0300

    Chage signal value functions and pokemon image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,10 +9,17 @@ export default component$(() => {
     <>
       <span class="text-2xl">Buscadr simple</span>
       <span class="text-9xl">{pokemonID.value}</span>
-      { /*TODO: crear imagen*/}
+      <img
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonID.value}.png`}
+        alt="Pokemon Sprite"
+        width={200}
+        height={200}
+
+
+      />
       <div class="mt-2">
-        <button class="btn btn-primary mr-2">Anterior</button>
-        <button class="btn btn-primary">Siguiente</button>
+        <button onClick$={() => pokemonID.value--} class="btn btn-primary mr-2">Anterior</button>
+        <button onClick$={() => pokemonID.value++} class="btn btn-primary">Siguiente</button>
       </div>
     </>
   );
