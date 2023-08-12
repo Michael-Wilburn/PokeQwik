6189de919bcfa6229913e2efe8beb9f6c067d3b4
Michael Wilburn <michael-mw@hotmail.com>
2023-08-12 16:55:40 -0300

    adding Link to navigate to routes

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -1,22 +1,28 @@
 import { component$ } from "@builder.io/qwik";
 import { Image } from '@unpic/qwik';
+import { Link } from "@builder.io/qwik-city";
 import './navbar.module.css'
-import Logo from '../../../../public/favicon.svg'
+import Logo from '../../../assets/logo/favicon.svg'
 
 export const Navbar = component$(() => {
 
     return (
         <nav class="flex flex-row justify-between items-center px-6">
-            <Image
-                src={Logo}
-                layout="constrained"
-                width={50}
-                height={50}
-                alt="Logo"
-            />
-            <ul>
+            <Link href="/">
+                <Image
+                    src={Logo}
+                    layout="constrained"
+                    width={50}
+                    height={50}
+                    alt="Logo"
+                />
+            </Link>
+            <ul class="flex">
+                <li class="mr-3">
+                    <Link href="/pokemons/list-ssr/">SSR-List</Link>
+                </li>
                 <li>
-                    <a href="#">Tuturiales</a>
+                    <Link href="/pokemons/list-client/">Client-Side</Link>
                 </li>
             </ul>
         </nav>
--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,5 +1,11 @@
 import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
     return <>Hola mundo SSR</>
 });
+
+
+export const head: DocumentHead = {
+    title: "SSR List",
+};
