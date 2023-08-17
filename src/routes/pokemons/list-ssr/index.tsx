b77240742bfa371109b1e2983bf6ef5d0d98066c
Michael Wilburn <michael-mw@hotmail.com>
2023-08-17 20:11:21 -0300

    Adding types for pokemon res

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,16 +1,17 @@
 import { component$ } from '@builder.io/qwik';
 import { type DocumentHead, Link, routeLoader$ } from "@builder.io/qwik-city";
 import axios from 'axios';
+import type { BasicPokemonInfo, PokemonListResponse } from '~/interfaces';
 
 
-export const usepokemonList = routeLoader$(async () => {
+export const usepokemonList = routeLoader$<BasicPokemonInfo[]>(async () => {
     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`);
-    const data = res.data.results;
-    return data
+    const data = res.data as PokemonListResponse
+    return data.results
 })
 
 export default component$(() => {
-    const pokemonResp = usepokemonList()
+    const pokemons = usepokemonList()
 
     return (
         <>
@@ -25,13 +26,12 @@ export default component$(() => {
                 <Link class="btn btn-primary mr-2">Siguientes</Link>
             </div>
             <div class="grid grid-cols-6 mt-5">
-                {/* {
-                    pokemonResp.value.map((pokemon: any) => {
-                        <div class="flex flex-col m-5 justify-center items-center">{pokemon.name}</div>
-                    })
-                } */}
-                {JSON.stringify(pokemonResp.value)}
+                {pokemons.value.map(pokemon => (
+                    <div key={pokemon.name} class="m-5 flex flex-col justify-center items-center">
+                        <span class="capitalize">{pokemon.name}</span>
+                    </div>
 
+                ))}
             </div>
         </>
     )
